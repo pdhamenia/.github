d8600f29497e810c9405e79bea77f2b48d5eaf68
pdhamenia <63222869+pdhamenia@users.noreply.github.com>
2021-09-14 12:48:59 +1000

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,34 @@
+The Terraform execution plan has been generated and is shown below.
+Resources are shown in alphabetical order for quick scanning. Green resources
+will be created (or destroyed and then created if an existing resource
+exists), yellow resources are being changed in-place, and red resources
+will be destroyed. Cyan entries are data sources to be read.
+
+Note: You didn't specify an "-out" parameter to save this plan, so when
+"apply" is called, Terraform can't guarantee this is what will execute.
+
++ aws_instance.web_svr
+    ami:                         "ami-1c47407f"
+    associate_public_ip_address: "<computed>"
+    availability_zone:           "<computed>"
+    ebs_block_device.#:          "<computed>"
+    ephemeral_block_device.#:    "<computed>"
+    instance_state:              "<computed>"
+    instance_type:               "t2.micro"
+    ipv6_addresses.#:            "<computed>"
+    key_name:                    "helloterraform"
+    network_interface_id:        "<computed>"
+    placement_group:             "<computed>"
+    private_dns:                 "<computed>"
+    private_ip:                  "<computed>"
+    public_dns:                  "<computed>"
+    public_ip:                   "<computed>"
+    root_block_device.#:         "<computed>"
+    security_groups.#:           "<computed>"
+    source_dest_check:           "true"
+    subnet_id:                   "<computed>"
+    tenancy:                     "<computed>"
+    vpc_security_group_ids.#:    "<computed>"
+
+
+Plan: 1 to add, 0 to change, 0 to destroy.
